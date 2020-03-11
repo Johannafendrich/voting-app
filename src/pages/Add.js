e1f82f61c9f5b38dcaf00fc381c1094b966d72da
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-11 16:47:42 +0100

    Add Router and prepare pages

---
+++ src/pages/Add.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Add() {
+  return (
+    <div>
+      <p>Add</p>
+      <Link to="/Vote">Vote</Link>
+    </div>
+  );
+}
+export default Add;
---
+++ src/pages/Result.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Result() {
+  return (
+    <div>
+      <p>Result</p>
+      <Link to="/Home">Home</Link>
+    </div>
+  );
+}
+export default Result;
