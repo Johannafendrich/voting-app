b4bc824c69eb4f1e155225bfa10a84da85bacdc9
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-12 11:55:57 +0100

    Add html and css form

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Card from "../components/Card";
+import "./Add.css";
 
 function Add() {
   return (
     <Card>
-      <p>Add</p>
+      <form className="form">
+        <input
+          className="form__input form__input-question"
+          type="text"
+          placeholder="Whats your Question?"
+        />
+        <input
+          className="form__input form__input-answer"
+          type="text"
+          placeholder="Fill in your first choice"
+        />
+        <input
+          className="form__input form__input-answer"
+          type="text"
+          placeholder="Fill in your second choice"
+        />
+        <input
+          className="form__input form__input-answer"
+          type="text"
+          placeholder="Fill in your third choice"
+        />
+      </form>
       <Link to="/Vote">Vote</Link>
     </Card>
   );
