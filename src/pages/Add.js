54b0ad3e7d2fa77f9a6edac5e733b1ce119a5fda
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-12 13:56:38 +0100

    Add poll

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -5,28 +5,60 @@ import "./Add.css";
 import Button from "../components/Button";
 
 function Add() {
+  const [question, setQuestion] = React.useState("");
+  const [firstAnswer, setFirstAnswer] = React.useState("");
+  const [secondAnswer, setSecondAnswer] = React.useState("");
+  const [thirdAnswer, setThirdAnswer] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(JSON.stringify(poll));
+  }
+  const poll = {
+    question: question,
+    firstAnswer: firstAnswer,
+    secondAnswer: secondAnswer,
+    thirdAnswer: thirdAnswer
+  };
+
   return (
     <Card>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <input
           className="form__input form__input-question"
           type="text"
           placeholder="Whats your Question?"
+          value={question}
+          onChange={event => {
+            setQuestion(event.target.value);
+          }}
         />
         <input
           className="form__input form__input-answer"
           type="text"
           placeholder="Fill in your first choice"
+          value={firstAnswer}
+          onChange={event => {
+            setFirstAnswer(event.target.value);
+          }}
         />
         <input
           className="form__input form__input-answer"
           type="text"
           placeholder="Fill in your second choice"
+          value={secondAnswer}
+          onChange={event => {
+            setSecondAnswer(event.target.value);
+          }}
         />
         <input
           className="form__input form__input-answer"
           type="text"
           placeholder="Fill in your third choice"
+          value={thirdAnswer}
+          onChange={event => {
+            setThirdAnswer(event.target.value);
+          }}
         />
         <Button>Create your Poll</Button>
       </form>
